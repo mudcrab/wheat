1141e9c525f66b7e3a12d9871d408e47c9b8cad1
Jevgeni Krais <jevgeni@pitfire.eu>
2014-07-14 16:54:54 +0300

    refactored the socket tests

--- test/socket.js
+++ test/socket.js
@@ -17,12 +17,6 @@ function setData(type, data)
 	return JSON.stringify({ type: type, data: data });
 };
 
-function handleMessage(data)
-{
-	var msg = JSON.parse(data);
-	Events.emit(msg.type, msg.data);
-};
-
 function closeConnections()
 {
 	setTimeout(function() {
@@ -32,57 +26,49 @@ function closeConnections()
 };
 
 describe('Socket client', function() {
-	it('should authenticate user', function(done) {
-		var client = new WebSocket(wsAddress);
+	var client;
 
+	beforeEach(function(done) {
+		client = new WebSocket(wsAddress);
 		client.on('open', function() {
 			client.send(setData('auth', { username: 'jk', password: 'asdf1234' }));
 
-			client.on('message', function(message) {
-				handleMessage(message);
+			client.on('message', function(data) {
+				var message = JSON.parse(data);
+				Events.emit(message.type, message.data);
 			});
+			done();
+		});
+	});
 
-			Events.on('auth', function(data) {
-				// console.log(data)
-				data.status.should.equal('authenticated');
-				client.close();
-				// closeConnections();
-				Events.off('auth');
-				done();
-			});
+	afterEach(function(done) {
+		client.close();
+		done();
+	});
+
+	it('should authenticate user', function(done) {
+		Events.on('auth', function(data) {
+			data.status.should.equal('authenticated');
+			Events.off('auth');
+			done();
 		});
 	});
 
 	it('should connect to an irc server', function(done) {
-		var client = new WebSocket(wsAddress);
+		var serverData = {
+			name: 'local2',
+			address: 'localhost'
+		};
 
-		client.on('open', function() {
-			client.send(setData('auth', { username: 'jk', password: 'asdf1234' }));
+		client.send(setData( 'connectServer', serverData ));
 
-			var serverData = {
-				name: 'local2',
-				address: 'localhost'
-			};
-
-			client.send(setData( 'connectServer', serverData ));
-
-			// TODO refacor to separate function?
-
-			client.on('message', function(message) {
-				handleMessage(message);
-			});
-
-
-
-			Events.on('connectServer', function(data) {
-				data.status.should.equal('connected');
-				closeConnections();
-				Events.off('connectServer');
-				done();
-			});
-
+		Events.on('connectServer', function(data) {
+			data.status.should.equal('connected');
+			Events.off('connectServer');
+			done();
 		});
 	});
+
 	it('should get a list of servers');
 	it('should get a list of channels');
 	it('should join a channel');
