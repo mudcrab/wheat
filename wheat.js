4646b64a66483fcd06f95b9b7801c4b810119875
Jevgeni Krais <jevgeni@pitfire.eu>
2014-07-18 15:43:47 +0300

    refactored wheat

--- wheat.js
+++ wheat.js
@@ -1,107 +1,116 @@
 var config = require('./config.js');
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ port: config.app.ports[process.env.NODE_ENV] || 1337 });
-var Events = require('minivents');
-var u = require('./users.js');
 var db = require('./db.js');
+var Events = require('minivents');
+var Log = require('./log.js');
+var Users = require('./users.js');
+var UsersManager = new Users();
+var helper = require('./helper.js');
 
-var Ev = new Events();
-var Users = new u();
-config.events = Ev;	
+// Configuration
 
-// console.log('WHEAT', config.app.ports[process.env.NODE_ENV])
+config.env = process.env.NODE_ENV || 'development';
+config.events = new Events();
+config.logger = new Log('log/' + config.env + '.log', { echo: false, timestamp: true });
+config.server = new WebSocketServer({ port: config.app.ports[config.env] });
 
-wss.on('connection', function(ws) {
-	ws.send(JSON.stringify({ type: 'connected' }));
-	ws.on('message', function(message) {
-		var m = JSON.parse(message);
-		Ev.emit('socket.' + m.type, { resp: m.data, socket: ws });
-		// console.log("[ SCK ] %s", m.type, m.data);
-	});
-	ws.on('close', function() {
-		Users.removeSocket(ws);
+config.logger.info("Started (%s) socket server on %d", config.env, config.app.ports[config.env]);
+
+// Set up socket server
+
+config.server.on('connection', function(client) {
+	client.send(helper.socketData('connected'));
+	config.logger.info('Client connected');
+
+	client.on('message', function(json) {
+		var message = JSON.parse(json);
+
+		if(message.type === 'auth')
+		{
+			config.events.emit('socket.' + message.type, {
+				response: message.data,
+				socket: client
+			});
+		}
+		else
+		{
+			var user = UsersManager.findUserBySocket(client);
+
+			if(user)
+				config.events.emit('socket.' + message.type, {
+					response: message.data,
+					socket: client,
+					user: user
+				});
+			else
+				config.logger.error("User socket not found");
+		}
+
+		config.logger.log("SOCKET - %s", message.type);
 	});
 });
 
-Ev.on('socket.auth', function(data) {
-	var user = Users.findUser(data.resp.username, data.resp.password, data.socket);
-	if(user)
+// Socket server event listeners
+
+config.events.on('socket.auth', function(data) {
+	if(UsersManager.findUser(data.response.username, data.response.password, data.socket))
 	{
-		// /*user.authenticate(data.socket);
-		data.socket.send(JSON.stringify({ type: 'auth', data: { status: 'authenticated' } }));
-		// data.socket.send(JSON.stringify({ type: 'chanlog', data: user.getServerLog('local') }));
-		data.socket.send(JSON.stringify({ type: 'chanlog', data: user.getServersLog() }));
-		// data.socket.send(JSON.stringify({ type: 'servers', data: user.getServers() }));
-		// console.log("%s authenticated", data.resp.username);
+		data.socket.send(helper.socketData('auth', { status: 'authenticated' }));
+		config.events.off('socket.auth');
+		config.logger.info("User %s authenticated", data.response.username);
 	}
 });
 
-Ev.on('socket.join', function(data) {
-	var user = Users.findUserBySocket(data.socket);
-	if(user)
-		user.joinChannel(data.resp.server, data.resp.channel);
-});
+/*
+	Client's irc actions
+*/
 
-Ev.on('socket.connectServer', function(data) {
-	var user = Users.findUserBySocket(data.socket);
-	// console.log(user)
-	if(user)
-		data.socket.send(JSON.stringify({ type: 'connectServer', data: { status: 'connected' } }))
-})
-
-Ev.on('socket.setNick', function(data) {
-	var user = Users.findUserBySocket(data.socket);
-	if(user)
-		user.setNick(data.resp.server, data.resp.nick);
-});
-
-Ev.on('socket.partChannel', function(data) {
-	var user = Users.findUserBySocket(data.socket);
-	if(user)
-		user.partChannel(data.resp.server, data.resp.channel);
-});
-
-Ev.on('socket.disconnect', function(data) {
-	var user = Users.findUserBySocket(data.socket);
-	if(user)
-		user.disconnect(data.server);
-});
-
-Ev.on('socket.say', function(data) {
-	Users.findUserBySocket(data.socket).sendMessage(data.resp.serverName, data.resp.channel, data.resp.message);
-});
-
-Ev.on('irc.say', function(data) {
-	data.users.forEach(function(user) {
-		user.send(JSON.stringify({ type: 'chanMsg', data: data.data  }));
+config.events.on('socket.irc.connect', function(data) {
+	data.user.addServer(data.response.name, data.response.address, data.response.nick, data.response.autojoin, function() {
+		data.socket.send(helper.socketData('connectServer', { status: 'connected' }));
 	});
-	/*Users.getAllUserSockets(data.user).forEach(function(socket) {
-		socket.send(JSON.stringify({ type: 'chanMsg', data: data.data }));
-	});*/
+	config.events.off('socket.irc.connect');
 });
 
-Ev.on('irc.connected', function(data) {
+config.events.on('socket.irc.disconnect', function(data) {
+	var status = data.user.disconnectServer(data.response.name);
+	data.socket.send(helper.socketData('disconnectServer', { status: status }));
+	config.events.off('socket.irc.disconnect');
+});
 
+config.events.on('socket.irc.join', function(data) {
+	data.user.joinChannel(data.response.server, data.response.channel); // TODO add callback?
+	config.events.off('socket.irc.join');
+});
+
+config.events.on('socket.irc.part', function(data) {
+	data.user.partChannel(data.response.server, data.response.channel); // TODO add callback?
+	config.events.off('socket.irc.part');
+});
+
+config.events.on('socket.irc.say', function(data) {
+	data.user.sendMessage(data.response.serverName, data.response.channel, data.response.message);
+	config.events.off('socket.irc.say');
+});
+
+config.events.on('socket.irc.servers', function(data) {
+	data.socket.send(helper.socketData('servers', { servers: data.user.getServers() }));
+});
+
+config.events.on('socket.irc.channels', function(data) {
+
+});
+
+/*
+	Server's irc actions
+*/
+
+config.events.on('irc.connected', function(data) {
 	data.users.forEach(function(user) {
-		user.send(JSON.stringify({ type: 'serverConnected', server: data.server.name }));
+		user.send(helper.socketData('serverConnected', { server: data.server.name }));
 	});
-
-	/*var u = Users.getUser('jk', 'asdf1234');
-	u.joinChannel('local', '#blah');
-
-	u.setNick('local', 'jk_');
-	u.send('local', '#blah', 'tere');
-	u.partChannel('local', '#blah');
-	u.send('local', 'kohvihoor', 'tere :)');
-	u.disconnect('local');
-
-	setTimeout(function() {
-
-	}, 2000);*/
 });
 
-Ev.on('irc.join', function(data) {
-   // console.log(data);
-});
+config.events.on('irc.join', function(data) {
 
-module.exports = { wss_: wss, db_: db };
+});
